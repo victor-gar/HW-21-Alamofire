b66fb2ec07d945154f6ed09c3f72875fdee45b14
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-13 16:27:48 +0600

    add viewModel

---
+++ MagicCards-Alamofire/Sources/ViewModels/CardsViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ViewModel.swift
+//  MagicCards-Alamofire
+//
+//  Created by Victor Garitskyu on 13.04.2023.
+//
+
+import Foundation
+import Alamofire
+
+// ViewModel
+class CardsViewModel {
+    
+    private let apiURL = "https://api.magicthegathering.io/v1/cards"
+    private var cards: [Card] = []
+    
+    var numberOfRows: Int {
+        return cards.count
+    }
+    
+    // Получение данных с API и сохранение в массив cards
+    func getCards(completion: @escaping () -> ()) {
+        AF.request(apiURL).validate().responseDecodable(of: CardList.self) { response in
+            guard let cardsList = response.value else { return }
+            self.cards = cardsList.cards
+            completion()
+        }
+    }
+    
+    // Получить карту по индексу из массива cards
+    func getCard(at index: Int) -> Card {
+        return cards[index]
+    }
+}
--- MagicCards-Alamofire/Sources/Views/Controller/MagicViewController.swift
+++ MagicCards-Alamofire/Sources/Views/Controller/MagicViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
 class MagicViewController: UIViewController {
 
