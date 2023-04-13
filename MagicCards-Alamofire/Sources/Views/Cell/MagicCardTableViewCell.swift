33d08e97f1f8c8c1e4b64abf52ff81674f4d2b65
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-13 16:45:49 +0600

    add cell

---
+++ MagicCards-Alamofire/Sources/Views/Cell/MagicCardTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MagicCardTableViewCell.swift
+//  MagicCards-Alamofire
+//
+//  Created by Victor Garitskyu on 13.04.2023.
+//
+
+import AlamofireImage
+import SnapKit
+import UIKit
+
+class MagicCardTableViewCell: UITableViewCell {
+    
+}
