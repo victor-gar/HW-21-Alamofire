7a55a4425393471893ec3204c47091ac82d174b4
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-13 16:22:52 +0600

    add model

---
+++ MagicCards-Alamofire/Sources/Models/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  MagicCards-Alamofire
+//
+//  Created by Victor Garitskyu on 13.04.2023.
+//
+
+import Foundation
+
+// Model
+struct Card: Codable {
+    let name: String
+    let imageUrl: String
+    let text: String
+}
